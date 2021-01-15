445e6e32f633dee1b6d5014034f4e4154793534b
sergiofagundesb <sergio.fagundesbarros@gmail.com>
2021-01-15 11:59:20 -0300

    adicionado botao de apagar ultimo caractere

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let primeiroUso = true;
-
+let apagouNumero = false;
+let numerosCalculadora = [];
 function limpaDisplay(){
     let display = document.getElementById("painel");
     
@@ -11,15 +12,15 @@ function limpaDisplay(){
 }
 
 function capturaNumero(primeiroNumero){
-    
-    
     let numero = document.getElementById(primeiroNumero).textContent;
     let display = document.getElementById("painel");
     if(display.textContent == "0"){
         display.innerText = "";
+    
         
     }
     display.innerText += numero;
+    numerosCalculadora.push(numero);
     // if(display.textContent == "0" && primeiroUso){
     //     
     //     primeiroUso = false;
@@ -29,4 +30,20 @@ function capturaNumero(primeiroNumero){
 
    return numero;
 }
+function limparUltimoNumero()
+{
+    apagouNumero = true;
+    let texto = document.getElementById("painel").textContent;
+    let retorno = texto.substring(0, texto.length - 1);
+    document.getElementById("painel").innerText = retorno;
+    console.log(`${retorno}`);
+}
+
+/* bloco de código que tá tirando número de 9 em 9, talvez seja útil para fazermos subtraindo de numero em numero
+tal como fazemos numa clculadora
+    apagouNumero = true;
+    let texto = document.getElementById("painel");
+    let ultimo  = texto.textContent.length-1 ;
+
+    document.getElementById("painel").innerText = texto.textContent-ultimo;*/
 
